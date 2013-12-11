8adfd7e23e8963e9a2a7a295a48dc64d95eb686d
Marion Knight <knight499@gmail.com>
2013-12-11 11:41:58 -0800

    killed it

--- primes.js
+++ primes.js
@@ -1,9 +1,7 @@
 exports.isPrime = function (n) {
   if (n == 1) return false;
   var s = Math.sqrt(n);
-  console.log( Math.sqrt(n) );
   for (var i = 2; i <= s; i++) {
-    console.log(n % i)
     if (n % i == 0) return false
   }
   return true
--- tests.js
+++ tests.js
@@ -1,16 +1,14 @@
 var primes = require('./primes');
 
 function assert(statement, expectation) {
-  console.log("expect", statement, "==", expectation, "to be true");
-  console.log(eval(statement) == expectation);
+  console.log("expect", statement, "to be", expectation);
+  console.log("...", eval(statement) == expectation);
 }
 
 assert("primes.isPrime(1)", false);
 assert("primes.isPrime(2)", true);
 assert("primes.isPrime(3)", true);
 assert("primes.isPrime(4)", false);
-assert("primes.isPrime(5)", true);
-assert("primes.isPrime(6)", false);
-assert("primes.isPrime(379)", true);
-assert("primes.isPrime(350)", false);
+assert("primes.isPrime(3571)", true);
+assert("primes.isPrime(3570)", false);
 
